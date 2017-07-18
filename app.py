3e687e505ce103ed30555405a5c32d0f235e428b
Jaison <jaisonsaji@gmail.com>
2017-07-18 23:06:46 +0530

    indentation changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import urllib.request, json
 from flask import Flask, abort, request
 
 
+
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def homepage():
@@ -26,41 +27,47 @@ def homepage():
 
 
 def ongoingEvents(data, etype):
-	val = ''
+	val = '['
 	for i in data['response']:
 		if etype != None:
 			if etype in i['challenge_type'].lower() and 'ongoing' in i['status'].lower():
-				val+=str("<h1>"+i['title']+"</h1><p>"+i['description']+"</p><p>"+i['url']+"</p><p>"+i['date']+"</p>")
+				val+=str('{ "title" : "'+i['title']+'", "description" : "'+i['description']+'", "url" : "'+i['url']+'", "date" : "'+i['date']+'" },')
 		else:
 			if 'ongoing' in i['status'].lower():
-				val+=str("<h1>"+i['title']+"</h1><p>"+i['description']+"</p><p>"+i['url']+"</p><p>"+i['date']+"</p>")
-	if val == '':
+				val+=str('{ "title" : "'+i['title']+'", "description" : "'+i['description']+'", "url" : "'+i['url']+'", "date" : "'+i['date']+'" },')
+	if val == '[':
 		val = 'No events found!'
+	else:
+		val = str(val[:-1]+"]")
 	return val
 
 def upcomingEvents(data, etype):
-	val = ''
+	val = '['
 	for i in data['response']:
 		if etype != None:
 			if etype in i['challenge_type'].lower() and 'upcoming' in i['status'].lower():
-				val+=str("<h1>"+i['title']+"</h1><p>"+i['description']+"</p><p>"+i['url']+"</p><p>"+i['date']+"</p>")
+				val+=str('{ "title" : "'+i['title']+'", "description" : "'+i['description']+'", "url" : "'+i['url']+'", "date" : "'+i['date']+'" },')
 		else:
 			if 'upcoming' in i['status'].lower():
-				val+=str("<h1>"+i['title']+"</h1><p>"+i['description']+"</p><p>"+i['url']+"</p><p>"+i['date']+"</p>")
-	if val == '':
+				val+=str('{ "title" : "'+i['title']+'", "description" : "'+i['description']+'", "url" : "'+i['url']+'", "date" : "'+i['date']+'" },')
+	if val == '[':
 		val = 'No events found!'
+	else:
+		val = str(val[:-1]+"]")
 	return val
 
 def allEvents(data, etype):
-	val = ''
+	val = '['
 	for i in data['response']:
 		if etype != None:
 			if etype in i['challenge_type'].lower():
-				val+=str("<h1>"+i['title']+"</h1><p>"+i['description']+"</p><p>"+i['url']+"</p><p>"+i['date']+"</p>")
+				val+=str('{ "title" : "'+i['title']+'", "description" : "'+i['description']+'", "url" : "'+i['url']+'", "date" : "'+i['date']+'" },')
 		else:
-			val+=str("<h1>"+i['title']+"</h1><p>"+i['description']+"</p><p>"+i['url']+"</p><p>"+i['date']+"</p>")
-	if val == '':
+			val+=str('{ "title" : "'+i['title']+'", "description" : "'+i['description']+'", "url" : "'+i['url']+'", "date" : "'+i['date']+'" },')
+	if val == '[':
 		val = 'No events found!'
+	else:
+		val = str(val[:-1]+"]")
 	return val
 
 
